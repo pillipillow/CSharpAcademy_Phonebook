36fc9bcbcdf81bd7cc7467114d259cfe8ce3526e
DANIA-DESKTOP\user <ainuldania@gmail.com>
2026-05-22 11:48:33 +0800

    Added Contact entity and PhonebookContext

---
+++ Phonebook/Model/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace Phonebook.Model;
+
+internal class Contact
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int PhoneNumber { get; set; }
+    public int EmailAddress { get; set; }
+}
---
+++ Phonebook/PhonebookContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Phonebook.Model;
+
+namespace Phonebook;
+
+internal class PhonebookContext : DbContext
+{
+    public DbSet<Contact> Contacts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=PhonebookDB;Trusted_Connection=True;");
+    }
+}
