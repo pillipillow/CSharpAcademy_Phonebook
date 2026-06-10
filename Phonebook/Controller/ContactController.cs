ab63601259d019e31b1dc70429514e9d01d0d1e7
DANIA-DESKTOP\user <ainuldania@gmail.com>
2026-06-10 17:10:28 +0800

    Added deleted and updated method

--- Phonebook/Controller/ContactController.cs
+++ Phonebook/Controller/ContactController.cs
@@ -40,31 +40,26 @@ internal class ContactController
     {
         Console.Clear();
         Console.WriteLine("---Add New Contact---");
-        Console.WriteLine("Insert new contact name: ");
+        Console.WriteLine("Insert new contact name (Insert 0 to return to main menu):");
         string name = helpers.CheckName();
 
         if (name == "0") return;
 
-        Console.WriteLine("Insert new contact phone number (00-0000-0000): ");
+        Console.WriteLine("Insert new contact phone number (00-0000-0000)(Insert 0 to return to main menu): ");
         string number = helpers.CheckPhoneNumber();
 
         if(number == "0") return;
 
-        Console.WriteLine("Insert new contact email address:");
+        Console.WriteLine("Insert new contact email address (Insert 0 to return to main menu):");
         string email = helpers.CheckEmail();
 
         dbmanager.AddContact(name, number, email);
         Console.WriteLine("\nNew Contact has been added in the Phonebook!");
 
-        Console.WriteLine("Would you like to add another contact?: (Y/N)");
+        Console.WriteLine("Would you like to add another contact?: (y/n)");
         string input = Console.ReadLine();
         if (!string.IsNullOrWhiteSpace(input) && input.ToLower() == "y")
             NewContact();
-        else
-        {
-            Console.WriteLine("Press enter to go back to the main menu.");
-            Console.ReadLine();
-        }
     }
 
     internal static void DeleteContact()
@@ -74,8 +69,75 @@ internal class ContactController
         Console.Clear();
         Console.WriteLine("---Delete Contact---");
 
-        Console.WriteLine("Press enter to go back to the main menu.");
-        Console.ReadLine();
+        Console.WriteLine("Insert the contact name (Insert 0 to return to main menu): ");
+        string name = helpers.CheckName();
+
+        if (name == "0") return;
+
+        contacts = dbmanager.FindContact(name);
+
+        if (contacts.Count == 0)
+        {
+            Console.WriteLine("Contact not found\n");
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("---Delete Contact---");
+            Console.WriteLine($"{contacts.Count} found\n");
+
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"ID: {contact.Id}");
+                Console.WriteLine($"Name: {contact.Name}");
+                Console.WriteLine($"Phone: {contact.PhoneNumber}");
+                Console.WriteLine($"E-mail: {contact.EmailAddress}");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Insert their ID to delete their contact (Insert 0 to return to main menu): ");
+            int id = helpers.CheckNumber();
+
+            if (id == 0) return;
+
+            List<Contact> foundContact = new List<Contact>();
+            foundContact = dbmanager.FindContactByIdAndName(id, name);
+
+
+            if (foundContact.Count == 0)
+            {
+                Console.WriteLine("ID doesn't match to any of the contact name.");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("---Delete Contact---");
+                foreach (var contact in foundContact)
+                {
+                    Console.WriteLine($"ID: {contact.Id}");
+                    Console.WriteLine($"Name: {contact.Name}");
+                    Console.WriteLine($"Phone: {contact.PhoneNumber}");
+                    Console.WriteLine($"E-mail: {contact.EmailAddress}");
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("Are you sure you want to delete this contact? (y/n)");
+                string confirmation = Console.ReadLine();
+
+                if (confirmation.Trim().ToLower() == "y")
+                {
+                    dbmanager.DeleteById(id);
+                    Console.WriteLine("Contact deleted sucessfully!");
+                }
+                else
+                    Console.WriteLine("Delete contact cancelled");
+            }
+        }
+
+        Console.WriteLine("\nWould you like to delete another contact?: (Y/N)");
+        string input = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(input) && input.ToLower() == "y")
+            DeleteContact();
 
     }
 
@@ -84,8 +146,101 @@ internal class ContactController
         Console.Clear();
         Console.WriteLine("---Update Contact---");
 
-        Console.WriteLine("Press enter to go back to the main menu.");
-        Console.ReadLine();
+        Console.WriteLine("Insert the contact name (Insert 0 to return to main menu): ");
+        string name = helpers.CheckName();
+
+        if (name == "0") return;
+
+        contacts = dbmanager.FindContact(name);
+
+        if (contacts.Count == 0)
+        {
+            Console.WriteLine("Contact not found\n");
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("---Update Contact---");
+            Console.WriteLine($"{contacts.Count} found\n");
+
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"ID: {contact.Id}");
+                Console.WriteLine($"Name: {contact.Name}");
+                Console.WriteLine($"Phone: {contact.PhoneNumber}");
+                Console.WriteLine($"E-mail: {contact.EmailAddress}");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Insert their ID to update their contact (Insert 0 to return to main menu): ");
+            int id = helpers.CheckNumber();
+
+            if (id == 0) return;
+
+            List<Contact> foundContact = new List<Contact>();
+            foundContact = dbmanager.FindContactByIdAndName(id, name);
+
+            if (foundContact.Count == 0)
+            {
+                Console.WriteLine("ID doesn't match to any of the contact name.");
+            }
+            else
+            {
+                var contact = foundContact[0];
+                bool isCompleted = false;
+
+                while (!isCompleted)
+                {
+                    Console.Clear();
+                    Console.WriteLine("---Update Contact---");
+                    Console.WriteLine($"Name: {contact.Name}");
+                    Console.WriteLine($"Phone: {contact.PhoneNumber}");
+                    Console.WriteLine($"E-mail: {contact.EmailAddress}");
+                    Console.WriteLine();
+                    Console.WriteLine("1 - Update name.");
+                    Console.WriteLine("2 - Update phone number.");
+                    Console.WriteLine("3 - Update email address.");
+                    Console.WriteLine("0 - Complete changes");
+
+                    string updateInput = Console.ReadLine();
+
+                    switch (updateInput)
+                    {
+                        case "0":
+                            isCompleted = true;
+                            break;
+                        case "1":
+                            Console.WriteLine("\nInsert new contact name (Insert 0 to return to keep): ");
+                            string newName = helpers.CheckName();
+                            if(newName != "0")
+                                contact.Name = newName;
+                            break;
+                        case "2":
+                            Console.WriteLine("\nInsert new contact phone number (00-0000-0000) (Insert 0 to return to keep): ");
+                            string number = helpers.CheckPhoneNumber();
+                            if (number != "0")
+                                contact.PhoneNumber = number;
+                            break;
+                        case "3":
+                            Console.WriteLine("Insert new contact email address (Insert 0 to return to keep):");
+                            string email = helpers.CheckEmail();
+                            if (email != "0")
+                                contact.EmailAddress = email;
+                            break;
+                    }
+
+                }
+
+                dbmanager.UpdateById(id, contact.Name, contact.PhoneNumber, contact.EmailAddress);
+                Console.WriteLine("Contact update sucessful!");
+
+            }
+        }
+
+        Console.WriteLine("\nWould you like to update another contact?: (Y/N)");
+        string input = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(input) && input.ToLower() == "y")
+            UpdateContact();
     }
 
     internal static void FindContact()
--- Phonebook/DatabaseManager.cs
+++ Phonebook/DatabaseManager.cs
@@ -1,4 +1,5 @@
-﻿using Phonebook.Model;
+﻿using Microsoft.EntityFrameworkCore;
+using Phonebook.Model;
 
 namespace Phonebook;
 
@@ -34,10 +35,42 @@ internal class DatabaseManager
     internal List<Contact> FindContact(string name)
     {
         using (var context = new PhonebookContext())
-        { 
-            var contact = context.Contacts.Where(n => n.Name.Contains(name)).OrderBy(n=>n);
-            return contact.ToList();  
+        {
+            var contact = context.Contacts.Where(c => c.Name.Contains(name)).OrderBy(n => n);
+            return contact.ToList();
         }
     }
 
+    internal List<Contact> FindContactByIdAndName(int id, string name)
+    {
+        using (var context = new PhonebookContext())
+        {
+            var contact = context.Contacts.Where(c => c.Id == id && c.Name.Contains(name));
+            return contact.ToList();
+        }
+    }
+
+    internal void DeleteById(int id)
+    {
+        using (var context = new PhonebookContext())
+        { 
+            var rowsDeleted = context.Contacts.Where(c => c.Id == id).ExecuteDelete();
+        }
+    
+    }
+
+    internal void UpdateById(int id, string name, string phoneNumber, string email)
+    {
+        using (var context = new PhonebookContext())
+        {
+            context.Contacts.Where(c => c.Id == id).ExecuteUpdate(setter => 
+            { 
+                setter.SetProperty(c=>c.Name, name);    
+                setter.SetProperty(c=>c.PhoneNumber, phoneNumber);
+                setter.SetProperty(c=>c.EmailAddress, email);
+            
+            });
+        }
+
+    }
 }
