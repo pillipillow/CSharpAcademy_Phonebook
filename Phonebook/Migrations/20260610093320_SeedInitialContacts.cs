58c36eecba03367fc441bc7a95ee4eca6f369fdf
DANIA-DESKTOP\user <ainuldania@gmail.com>
2026-06-10 17:56:26 +0800

    Added seed contacts

---
+++ Phonebook/Migrations/20260610093320_SeedInitialContacts.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Phonebook.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialContacts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "Id", "EmailAddress", "Name", "PhoneNumber" },
+                values: new object[,]
+                {
+                    { 1, "johnDoe@gmail.com", "John Doe", "110000111" },
+                    { 2, "john.doe@gmail.com", "John Doe", "114345111" },
+                    { 3, "maryjane@gmail.com", "Mary Jane", "110500631" },
+                    { 4, "alicegreen@gmail.com", "Alice Green", "120305141" },
+                    { 5, "bobvance@gmail.com", "Bob Vance", "120775991" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
--- Phonebook/PhonebookContext.cs
+++ Phonebook/PhonebookContext.cs
@@ -11,4 +11,17 @@ internal class PhonebookContext : DbContext
     {
         optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=PhonebookDB;Trusted_Connection=True;");
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Contact>().HasData(
+            new Contact { Id = 1, Name = "John Doe", PhoneNumber = "110000111", EmailAddress = "johnDoe@gmail.com"},
+            new Contact { Id = 2, Name = "John Doe", PhoneNumber = "114345111", EmailAddress = "john.doe@gmail.com"},
+            new Contact { Id = 3, Name = "Mary Jane", PhoneNumber = "110500631", EmailAddress = "maryjane@gmail.com"},
+            new Contact { Id = 4, Name = "Alice Green", PhoneNumber = "120305141", EmailAddress = "alicegreen@gmail.com"},
+            new Contact { Id = 5, Name = "Bob Vance", PhoneNumber = "120775991", EmailAddress = "bobvance@gmail.com" }
+        );
+    }
 }
